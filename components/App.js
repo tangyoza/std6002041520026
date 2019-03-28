3e0a4e7b752b36afb960ec9862658190067cefa7
กันตพงศ์ เฉลิมกิจวณิชย์ <s6002041520026@Computers-Mac-mini.local>
2019-03-28 14:26:39 +0700

    Using Header component on App.js

--- components/App.js
+++ components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from './Header';
 
 class App extends React.Component {
     render() {
         return (
             <View>
+                <Header title="Albums LV3"/>
                 <Text  style={{ fontSize:32 }} >Kantapong Chaloemkitwanit S60020141520026</Text>
             </View>
         );
