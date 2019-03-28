4f3ccc6200becf72fac92a800b49ece4d1673a98
กันตพงศ์ เฉลิมกิจวณิชย์ <s6002041520026@Computers-Mac-mini.local>
2019-03-28 15:22:13 +0700

    Create Card Components

---
+++ components/CardSection.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class CardSection extends Component {
+    render() {
+        return(
+            <View>
+                <Text>AreisMS</Text>
+            </View>
+        );
+    }
+
+}
+
+export default CardSection;
