396209787f62e8ebef612d89243fbcd75e0c5b83
กันตพงศ์ เฉลิมกิจวณิชย์ <s6002041520026@Computers-Mac-mini.local>
2019-03-28 14:25:51 +0700

    Header Component

---
+++ components/Header.js
@@ -0,0 +1,47 @@
+import React, { Component} from 'React';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+class Header extends Component {
+    render(){
+        return (
+            <View style={styles.viewStyle}>
+                <Text style={styles.headbar}>{this.props.title}</Text>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    headbar: {
+        color: '#FFF',
+        fontSize: 30,
+
+    },
+    headpic: {
+        width: 40,
+        height: 40,
+        position: 'absolute',
+        right: 7.5,
+        borderRadius: 5
+        
+
+    },
+    viewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 5,
+        //borderRadius: 5,
+        borderColor: '#222',
+        backgroundColor: '#000',
+        shadowColor: '#C5C',
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 1,
+        elevation: 10,
+        position: 'relative'
+    }
+    
+});
+
+export default Header;
