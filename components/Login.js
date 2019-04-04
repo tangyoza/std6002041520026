ddcab5fcf4f90dd333c01e622e865190ea951906
กันตพงศ์ เฉลิมกิจวณิชย์ <s6002041520026@Computers-Mac-mini.local>
2019-04-04 17:19:50 +0700

    add Asyncstorage and save Token

--- components/Login.js
+++ components/Login.js
@@ -1,8 +1,7 @@
 // import libraly
 import React, { Component } from 'react';
-import {View, Text, TextInput, Image, Button } from 'react-native';
+import {View, Text, TextInput, Image, Button, AsyncStorage } from 'react-native';
 import axios from 'axios';
-
 
 // write componenet
 class Login extends Component{
@@ -15,6 +14,8 @@ class Login extends Component{
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
         this.onPressLogin = this.onPressLogin.bind(this);
+        this.onPressCheck = this.onPressCheck.bind(this);
+        const myToken ='';
     }
     onChangeEmail(e){
         this.setState({ email: e});
@@ -29,13 +30,36 @@ class Login extends Component{
         const url = "http://128.199.240.120:9999/api/auth/login";
         
         axios.post(url,this.state)
-        .then(function (resp){
+        .then(async function (resp){
             console.log(resp.data.data.token)
-            alert(resp.data.data.token);
+            //alert(resp.data.data.token);
+            alert("Login Succecfuly");
+            {
+                try {
+                  await AsyncStorage.setItem('token' , resp.data.data.token);
+                  console.log("save token succ")
+                } catch (error) {
+                  console.log(error)
+                }
+              };
+            
         })
         .catch(function (error){
             console.log(error)
         })
+    }
+    async onPressCheck(){
+        console.log("Check ACT");
+        {
+            try {
+              myToken = await AsyncStorage.getItem('token');
+              if (myToken !== null) {
+                console.log(myToken);
+              }
+            } catch (error) {
+              console.log(error);
+            }
+          };
     }
     render(){
         return(
@@ -65,6 +89,13 @@ class Login extends Component{
                     title="Login"
                     color="#64a2ff"
                     onPress={this.onPressLogin}
+
+
+                />
+                <Button
+                    title="Check"
+                    color="#64a2ff"
+                    onPress={this.onPressCheck}
 
 
                 />
